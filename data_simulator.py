07db627bad88b36cfa95d8d08c67291143a82cf0
Sara Mendes <sara.ferraz.mendes@gmail.com>
2016-06-09 23:44:32 +0200

    addedsimulator

---
+++ data_simulator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+#random sample generation for retinal concentration
+
+import random
+import numpy as np
+
+
+outfile = open( "data.txt","w" )
+
+
+data = [ ]  
+Retinal = [ 0, 5, 10, 15 ] 
+
+
+for Concentration in Retinal:
+
+	data = random.sample( range( 0, 181 ), 31 )
+	
+
+	#print ( "{} {}\n".format( Concentration, data ))
+
+		
+	outfile.write( "{} {}\n".format( Concentration, data ))
+
+outfile.close()

a9be4ec548dfeca2171522247a86bf4a9d89e91e
Sara Mendes <sara.ferraz.mendes@gmail.com>
2016-06-09 11:45:28 +0200

    added for loop for concentrations

---
+++ data_simulaton.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+#From sample
+
+import random
+
+
+Retinal = [ 0, 5, 10, 15, 20 ] 
+for Concentration in Retinal:
+	print ()
+
+
+my_randoms=[]    
+
+for i in range (30):    
+    my_randoms.append(random.randrange(1,181))  
+print (my_randoms) 
--- normal_data.py
+++ normal_data.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
 
-import matplotlib.pyplot as plt
 
-numpy.random.normal( 0.0, 1.0, None )
+np.random.normal( 30, 2, None )

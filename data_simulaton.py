b9293ac01ace47d05d359a06c7d41033f5a408a1
Sara Mendes <sara.ferraz.mendes@gmail.com>
2016-06-09 15:26:13 +0200

    added numpy

--- data_simulaton.py
+++ data_simulaton.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python
 
-#From sample
+#random sample generation for retinal concentration
 
 import random
 import sys
+import numpy as np
 
-sys.stdout = open("test.ods","w")
+#np.random.randn( 4, 5)
+
+
+#sys.stdout = open("test.ods","w")
 
 my_randoms=[]  
 
 
-Retinal = [ 0, 5, 10 ] 
+Retinal = [ 0, 5, 10, 15 ] 
 
 
 for Concentration in Retinal:
 
 	for i in range ( 1 ) :   
-		my_randoms=random.sample(range(0, 181), 5)
+		my_randoms=random.sample(range(0, 181), 31)
 		#my_randoms.append(random.randrange( 0, 181 ))  #1 to 180 minutes and number of observations
 
 	 
 	print ( "Concentration {}: {} minutes survival".format(Concentration, my_randoms) )
+
+#generating normal distribution sample
 

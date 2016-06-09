5daf6e6c7a39d231042e2d51a830d224389064e8
Sara Mendes <sara.ferraz.mendes@gmail.com>
2016-06-09 14:41:06 +0200

    random generation working

--- data_simulaton.py
+++ data_simulaton.py
@@ -7,11 +7,15 @@ import random
 my_randoms=[]  
 
 
-Retinal = [ 0, 5, 10, 15, 20 ] 
+Retinal = [ 0, 5, 10 ] 
 
 
 for Concentration in Retinal:
-	for i in range (30):   
-		my_randoms.append(random.randrange(0,181))  #1 to 180 h
-	print (my_randoms) 
-	print ( "Concentration {}: {}minutes survival".format(Concentration, my_randoms).strip(  )  )
+
+	for i in range ( 1 ) :   
+		my_randoms=random.sample(range(0, 181), 5)
+		#my_randoms.append(random.randrange( 0, 181 ))  #1 to 180 minutes
+
+	 
+	print ( "Concentration {}: {} minutes survival".format(Concentration, my_randoms) )
+
